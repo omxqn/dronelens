94c8724f3dabfe6fdc17ed1790ca33ef230bf442
Azzam <48726420+omxqn@users.noreply.github.com>
2025-04-19 17:01:39 +0400

    Update settings.py

--- DroneLens/settings.py
+++ DroneLens/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = 'core/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
